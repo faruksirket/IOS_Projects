a5a5c09723139cb4f131a98e01e195c7375bbd99
Faruk Şirket <47524210+faruksirket@users.noreply.github.com>
2022-06-14 15:38:21 +0300

    Add files via upload

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by faruk sirket on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
