048c6de1ddc7486884ab3d2ccc41b30fead2fc36
Faruk Şirket <47524210+faruksirket@users.noreply.github.com>
2022-06-14 15:37:44 +0300

    Add files via upload

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by faruk sirket on 6.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
